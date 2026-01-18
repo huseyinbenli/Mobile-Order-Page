aa044f8b20b66c219be1a2034b0003778705482f
huseyinbenli <huseyinbenli@outlook.fr>
2026-01-18 22:02:05 +0100

    function to render header

---
+++ functions/renderHeader.js
@@ -0,0 +1,10 @@
+export function renderHeader() {
+  return `
+        <div class='header'>
+        <div class='header-text-wrapper'>
+        <h1 class='header-title'>jimmy's diner</h1>
+        <p class='header-desc-text'>The best burgers and pizzas in town.</p>
+        </div>
+        </div>
+    `;
+}
