c16de444e6e9db0affa0140d11bd13cee8d6a374
huseyinbenli <huseyinbenli@outlook.fr>
2026-01-20 13:27:29 +0100

    handled a situation where the ordered item remains in the cart

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 import { menuArray } from "./data.js";
 import { renderMenu } from "./functions/renderMenu.js";
 import { renderHeader } from "./functions/renderHeader.js";
-import { addToCart, removeItem } from "./functions/handleCart.js";
+import {
+  addToCart,
+  removeItem,
+  renderCart,
+  cart,
+} from "./functions/handleCart.js";
 import { svg } from "./image/svg.js";
 
 const orderForm = document.getElementById("order-form");
@@ -12,7 +17,7 @@ const header = (document.getElementById("header-container").innerHTML =
 const menu = (document.getElementById("menu").innerHTML +=
   renderMenu(menuArray));
 
-document.addEventListener("click", (e) => {
+const documentEvent = document.addEventListener("click", (e) => {
   if (e.target.dataset.id) {
     addToCart(e.target.dataset.id);
   } else if (e.target.dataset.removeid) {
@@ -30,6 +35,42 @@ orderForm.addEventListener("submit", (e) => {
   const orderFormData = new FormData(orderForm);
   const fullName = orderFormData.get("fullName");
   const formSectionEl = document.querySelector(".form-section");
+  const formSectionWrapper = document.getElementById("form-section-wrapper");
+  const formHtmlEl = `
+        <section class="form-section">
+          <form id="order-form" class="form">
+            <h3>Enter card details</h3>
+            <input
+              type="text"
+              name="fullName"
+              placeholder="Enter your name"
+              required
+            />
+
+            <input
+              pattern="^[0-9]+$"
+              maxlength="12"
+              type="text"
+              name="cardNumber"
+              placeholder="Enter card number"
+              required
+            />
+
+            <input
+              type="text"
+              pattern="^[0-9]+$"
+              maxlength="3"
+              name="cvvNUmber"
+              placeholder="Enter CVV"
+              required
+            />
+            <button type="submit" class="pay-btn" id="pay-btn">Pay</button>
+            <button class="cancel-btn" class="cancel-btn" id="cancel-btn">
+              Cancel
+            </button>
+          </form>
+        </section>
+       `;
 
   setTimeout(() => {
     formSectionEl.innerHTML = `
@@ -51,5 +92,8 @@ orderForm.addEventListener("submit", (e) => {
                 Thanks, ${fullName}!Your order is on it's way!
             </p>
         </div>`;
+    formSectionWrapper.innerHTML = formHtmlEl;
+    cart.pop();
+    orderFormData.delete("fullName");
   }, 4000);
 });
